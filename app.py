5d506d6e39de2e204ff4fe6e31077909e5737a84
Brent Bradbury <brent@brentium.com>
2014-10-02 03:53:59 +0000

    iFix PORT

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +7,4 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port = os.getenv('PORT',5000) )
